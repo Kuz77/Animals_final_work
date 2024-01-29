45e66461da73c3f6c5d7862cc32aff8681bef41c
Ilya <kuzovlev92@mail.ru>
2024-01-29 20:16:05 +0300

    счетчик -1

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -1,21 +1,44 @@
 package org.example;
 
+//public class Counter implements AutoCloseable {
+//    private int count;
+//    public Counter() {
+//        count = 0;
+//    }
+//    public void add() {
+//        count++;
+//    }
+//    public int getCount() {
+//        return count;
+//    }
+//    @Override
+//    public void close() throws Exception {
+//        if (count == 0) {
+//            throw new Exception("Ошибка  работа с объектом\n" +
+//                    "типа счетчик была не в ресурсном try и/или ресурс остался открыт.");
+//        }
+//    }
+//}
+
 public class Counter implements AutoCloseable {
     private int count;
+
     public Counter() {
-        count = 0;
+        this.count = 0-1;
     }
+
     public void add() {
         count++;
     }
+
     public int getCount() {
         return count;
     }
+
     @Override
     public void close() throws Exception {
         if (count == 0) {
-            throw new Exception("Ошибка  работа с объектом\n" +
-                    "типа счетчик была не в ресурсном try и/или ресурс остался открыт.");
+            throw new Exception("Ошибка: работа с объектом типа счетчик была не в ресурсном try или ресурс остался открыт.");
         }
     }
 }
